285624b369b519320cbbd6e0bbabf48863d8afc9
Sergei Liutov <sliutov@swaxis.com>
2017-06-26 09:37:05 +0300

    /docs for git pages

---
+++ docs/demo/Assets/meta/assetCountMap.js
@@ -0,0 +1,6 @@
+assetCountMap = {
+  "Assets/scene/credits.bin": 4,
+  "Temp/scene_editor.bin": 27,
+  "Assets/scene/game.bin": 40,
+  "Assets/scene/mmenu.bin": 27
+};
---
+++ docs/demo/Assets/meta/soundDuration.js
@@ -0,0 +1,16 @@
+soundDurationMap = {
+  "Assets/sounds/lazer-beam.mp3.bin": 0.09870748299319727,
+  "Assets/sounds/ship-crash.mp3.bin": 0.9828798185941043,
+  "Assets/sounds/aster-crash.mp3.bin": 1.0187755102040816,
+  "Assets/sounds/aster-hit.mp3.bin": 0.4353514739229025,
+  "Assets/sounds/ufo_fly.mp3.bin": 3.5,
+  "Assets/sounds/shop_buy_failed.mp3.bin": 0.5283673469387755,
+  "Assets/sounds/shop_buy_ok.mp3.bin": 0.710702947845805,
+  "Assets/sounds/teleport.mp3.bin": 0.33519274376417235,
+  "Assets/sounds/ship_engine.mp3.bin": 2.5309977324263038,
+  "Assets/sounds/sonic_wave.mp3.bin": 1.079546485260771,
+  "Assets/sounds/ping.mp3.bin": 1.5904761904761904,
+  "Assets/sounds/getbonus.mp3.bin": 0.49848072562358275,
+  "Assets/sounds/getgem.mp3.bin": 0.251156462585034,
+  "Assets/sounds/gnum-engine.mp3.bin": 2.3478004535147394
+};
