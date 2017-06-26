279bc80c75e5421b1364aa6891b21e9433495ec2
Sergei Liutov <sliutov@swaxis.com>
2017-06-26 09:54:20 +0300

    docs/assets

---
+++ Scripts/Option_DropDown_Drag.js
@@ -0,0 +1,17 @@
+// define a user behaviour
+var Option_DropDown_Drag = qc.defineBehaviour('qc.engine.Option_DropDown_Drag', qc.Behaviour, function() {
+    // need this behaviour be scheduled in editor
+    //this.runInEditor = true;
+}, {
+    // fields need to be serialized
+});
+
+// Called when the script instance is being loaded.
+//Option_DropDown_Drag.prototype.awake = function() {
+//
+//};
+
+// Called every frame, if the behaviour is enabled.
+//Option_DropDown_Drag.prototype.update = function() {
+//
+//};
