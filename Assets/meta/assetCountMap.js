a0078ba85348b7acad1d7ed71a37717f27738cee
Sergei Liutov <sliutov@swaxis.com>
2017-06-28 08:16:20 +0300

    fix: lost cursor at credits

--- Assets/meta/assetCountMap.js
+++ Assets/meta/assetCountMap.js
@@ -1,6 +1,4 @@
 assetCountMap = {
-  "Assets/scene/credits.bin": 4,
-  "Temp/scene_editor.bin": 27,
-  "Assets/scene/game.bin": 40,
-  "Assets/scene/mmenu.bin": 27
+  "Assets/scene/credits.bin": 5,
+  "Temp/scene_editor.bin": 5
 };
